c2e6a168c66e064a993d85013a31d387e8487e96
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2023-07-11 22:06:34 -0500

    Refine server configuration

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,8 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 const TrackAPI = require("./datasources/track-api");
-
-const typeDefs = require("./schema");
 
 async function startApolloServer() {
   const server = new ApolloServer({
@@ -13,7 +12,7 @@ async function startApolloServer() {
   const { url } = await startStandaloneServer(server, {
     context: async () => {
       const { cache } = server;
-      // this obect becomes our resolver's contextValue,
+      // this object becomes our resolver's contextValue,
       // the third positional argument
       // the name dataSources is convention
       return {
